478ff5551b1f9e88389bf0fc7c3bf6d8325d41b5
Darthpbal <Ronquillo.paul@gmail.com>
2016-11-29 20:21:38 -0700

    formatting

--- basys_comboLock.v
+++ basys_comboLock.v
@@ -4,17 +4,17 @@ be more intuitive.
 */
 
 module basys (
-  input [15:0] swt,   //basys board switches
-  input clk,          //basys board clock
-  input btnU,         //up button on basys board
-  input btnD,         //up button on basys board
-  input btnL,         //up button on basys board
-  input btnR,         //up button on basys board
-  input btnC,         //up button on basys board
-  output [3:0] segEn, //basys seven segment enable bus
-  output segDec,         //seven segment decimal point
-  output [6:0] sevSeg,//seven segment signals
-  output [15:0] led   //basys board LED array
-  );
+    input [15:0] swt,   //basys board switches
+    input clk,          //basys board clock
+    input btnU,         //up button on basys board
+    input btnD,         //up button on basys board
+    input btnL,         //up button on basys board
+    input btnR,         //up button on basys board
+    input btnC,         //up button on basys board
+    output [3:0] segEn, //basys seven segment enable bus
+    output segDec,         //seven segment decimal point
+    output [6:0] sevSeg,//seven segment signals
+    output [15:0] led   //basys board LED array
+    );
 
 endmodule

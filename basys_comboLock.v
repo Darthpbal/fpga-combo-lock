df7619e59433e1d419140fc1b88be83391ea3f55
Darthpbal <Ronquillo.paul@gmail.com>
2016-12-07 01:20:16 -0700

    added components for debouncing a slow clock

--- basys_comboLock.v
+++ basys_comboLock.v
@@ -14,16 +14,36 @@ module basys (
         input btnC,         //up button on basys board
         output [3:0] segEn, //basys seven segment enable bus
         // output segDec,         //seven segment decimal point
-        output [6:0] sevSeg,//seven segment signals
+        output [6:0] seg,//seven segment signals
         // output [2:0] numCount,
         output [1:0] led   //basys board LED array
     );
 
+    wire slow_clk;
+    slowclock forInput(
+        .clk(clk),
+        .reset(btnD),
+        .slow_clk(slow_clk)
+    );
+
+    reg leftTrig, rightTrig;
+
+    wire centerTrig; assign centerTrig = btnC & clk;
+
+    always @ ( clk ) begin
+        if(btnL) leftTrig <= 1;
+        else leftTrig <= 0;
+        if(btnR) rightTrig <= 1;
+        else rightTrig <= 0;
+        // leftTrig <= (btnL)? 1: 0;
+        // rightTrig <= (btnR)? 1: 0;
+        // centerTrig <= (btnC)? btnC & clk: 0;
+    end
 
     wire [3:0] numSelect;
     upDownCount hexValSelector(
-        .up(btnR),
-        .down(btnL),
+        .up(rightTrig),
+        .down(leftTrig),
         .rst(btnD),
         .numOut(numSelect)
     );
@@ -31,7 +51,7 @@ module basys (
     wire [15:0] pinCode;
     fourIn16OutShiftReg shiftInPin(
         .in(numSelect),
-        .trig(btnC),
+        .trig(centerTrig),
         .rst(btnD),
         .out(pinCode)
     );
@@ -44,20 +64,20 @@ module basys (
         .disp1(pinCode[7:4]),
         .disp0(numSelect),
         .segEn(segEn),
-        .seg(sevSeg)
+        .seg(seg)
     );
 
     wire [1:0] numCount;
     wire flag;
     countTo4 triggerMachineOn4(
-        .trig(btnC),
+        .trig(centerTrig),
         .rst(btnD),
         .count(numCount),
         .flag(flag)
     );
 
     comboLockStateMachine stateMachine(
-        .pinCode(pinCode),
+        .pinCode({pinCode[15:4], numSelect}),
         .trig(flag),
         .lock(btnU),
         .rst(btnD),
